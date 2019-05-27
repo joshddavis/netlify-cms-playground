e5e5ccdce334ed0977875a9394679b16d6efef10
Joshua Davis <josh.oasis@gmail.com>
2019-05-27 14:41:48 -0400

    new manual post

---
+++ content/blog/2019-05-27-manual-posts.md
@@ -0,0 +1,6 @@
+---
+title: Manual Post!
+date: 2019-05-27T18:16:28.231Z
+description: A new post manually created by josh testing out the CMS
+---
+Will adding docment here get it to show in netlify?
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,19 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
+
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
@@ -52,15 +65,4 @@ exports.createPages = ({ graphql, actions }) => {
   })
 }
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
-}
